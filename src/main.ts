603d6a894c7d565c9000843bb11ef81592dc9fda
ctabares06 <ctr9120@gmail.com>
2026-05-03 07:29:14 -0500

    i was dumber, now im just dumb

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:80'],
     credentials: true,
   });
   // app.useGlobalPipes(new DtoValidationPipe());
